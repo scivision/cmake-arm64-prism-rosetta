aec0c7aeb6c65f21d2c4c82460c31871c14e4fcd
scivision <scivision@users.noreply.github.com>
2026-02-10 12:03:18 -0500

    doc

--- is_prism.py
+++ is_prism.py
@@ -3,6 +3,35 @@
 Python ctypes and system API calls didn't work reliably and didn't discern
 the executable types sufficiently to detect Prism.
 We found that env vars were simple and reliable.
+
+For example, with MSYS2 python
+UCRT64:
+
+sys.platform → win32
+sys.executable → C:/msys64/ucrt64/bin/python.exe
+platform.architecture() → ('64bit', 'WindowsPE')
+platform.machine() → ARM64
+platform.processor() → ARMv8 (64-bit) Family 8 Model 0 Revision   0, QEMU
+platform.uname() → uname_result(system='Windows', node='', release='11', version='10.0.26200', machine='ARM64')
+os.environ.get('PROCESSOR_ARCHITECTURE') → AMD64
+os.environ.get('PROCESSOR_ARCHITEW6432') → None
+GetCurrentProcess() handle → -1
+Direct IsWow64Process2 success? → 0
+  GetLastError → 6
+
+CLANGARM64:
+sys.platform → win32
+sys.executable → C:/msys64/clangarm64/bin/python.exe
+platform.architecture() → ('64bit', 'WindowsPE')
+platform.machine() → ARM64
+platform.processor() → ARMv8 (64-bit) Family 8 Model 0 Revision   0, QEMU
+platform.uname() → uname_result(system='Windows', node='', release='11', version='10.0.26200', machine='ARM64')
+os.environ.get('PROCESSOR_ARCHITECTURE') → ARM64
+os.environ.get('PROCESSOR_ARCHITEW6432') → None
+GetCurrentProcess() handle → -1
+Direct IsWow64Process2 success? → 1
+  process machine → 0x0
+  native machine  → 0xaa64
 """
 
 import os
